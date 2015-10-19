13182ef2a21d7f90672216e0dea1e3cd7a973e3f
Haohan Xie <haohanx@umich.edu>
2015-10-18 21:45:59 -0400

    boring little things Haha

--- 493 hw5/color-reverse.js
+++ 493 hw5/color-reverse.js
@@ -2,9 +2,8 @@ function drawHouse() {
         var canv = document.getElementById("myCanvas");
         canv.width = 300;
         canv.height = 300;
-        if(document.getElementById("draw").value=="0"){
-            console.log(1111)
-            document.getElementById("draw").value = 1;
+        var count = parseInt(document.getElementById("draw").value);
+        if(count%2 == 1){ 
             var house = canv.getContext("2d");
             //large rectangle
             house.fillStyle = "blue";
@@ -37,56 +36,20 @@ function drawHouse() {
             house.lineTo(110,50);
             house.closePath();
             house.fill();
+            count += 1;
+            document.getElementById("draw").value = count;
+            console.log(count);
             
-        }else if(document.getElementById("draw").value =="1"){
-            console.log(22222)
-            document.getElementById("draw").value = 2;
-           var house = canv.getContext("2d");
-            //large rectangle
-            house.fillStyle = "red";
-            house.fillRect(10,100,200,100);
-            //door
-            house.fillStyle = "black";
-            house.fillRect(100,140,30,60);
-            //wall
-            house.fillStyle = "blue";
-            house.beginPath();
-            house.moveTo(210,200);
-            house.lineTo(230,185);
-            house.lineTo(230,85);
-            house.lineTo(210,100);
-            house.closePath();
-            house.fill();
-            //roof1
-            house.fillStyle="blue";
-            house.beginPath();
-            house.moveTo(10,100);
-            house.lineTo(210,100);
-            house.lineTo(110,50)
-            house.closePath();
-            house.fill();
-            //roof2
-            house.fillStyle="red";
-            house.beginPath();
-            house.moveTo(210,100);
-            house.lineTo(230,85);
-            house.lineTo(110,50);
-            house.closePath();
-            house.fill();
-
         }else{
-            console.log(333);
-            document.getElementById("draw").value = 0;
-            document.getElementById("myCanvas").style.border= "10px solid red";
             var house = canv.getContext("2d");
             //large rectangle
-            house.fillStyle = "blue";
+            house.fillStyle = "red";
             house.fillRect(10,100,200,100);
             //door
             house.fillStyle = "black";
             house.fillRect(100,140,30,60);
             //wall
-            house.fillStyle = "red";
+            house.fillStyle = "blue";
             house.beginPath();
             house.moveTo(210,200);
             house.lineTo(230,185);
@@ -95,7 +58,7 @@ function drawHouse() {
             house.closePath();
             house.fill();
             //roof1
-            house.fillStyle="red";
+            house.fillStyle="blue";
             house.beginPath();
             house.moveTo(10,100);
             house.lineTo(210,100);
@@ -103,15 +66,21 @@ function drawHouse() {
             house.closePath();
             house.fill();
             //roof2
-            house.fillStyle="blue";
+            house.fillStyle="red";
             house.beginPath();
             house.moveTo(210,100);
             house.lineTo(230,85);
             house.lineTo(110,50);
             house.closePath();
             house.fill();
-            alert("The house survived 3 blows");
-            document.getElementById("myCanvas").style.border= "2px solid black";
-            house.clearRect(0,0,300,300)
-        };
-    }
+            count +=1;
+            document.getElementById("draw").value = count;
+            console.log(count);
+            
+        }
+        if(count%3 == 1){
+                document.getElementById("myCanvas").style.border= "20px solid red";
+                alert("The house survived 3 blows");
+                
+            }
+}
